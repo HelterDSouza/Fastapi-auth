36ac269164972da5ede7a20573530849a91860e2
helter daniel <helterdaniel@hotmail.com>
2022-03-26 23:10:58 -0300

    ✨ feat(app/db): adiciona conexão ao db
    adiciona conexão ao banco de dados postgreSQL

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
 
 from app.core import settings
+from app.core.events import create_start_app_handler, create_stop_app_handler
 
 
 def get_application() -> FastAPI:
     application = FastAPI(**settings.fastapi_kwargs)
 
-
+    application.add_event_handler(
+        "startup", create_start_app_handler(application, settings)
+    )
+    application.add_event_handler(
+        "shutdown",
+        create_stop_app_handler(application),
+    )
     return application
 
 
