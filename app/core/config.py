3746dc4e9a3777af5d00d5f80efbe2f5b3a45fa0
helter daniel <helterdaniel@hotmail.com>
2022-03-26 22:48:29 -0300

    configura ambiente de desenvolvimento

---
+++ app/core/config.py
@@ -0,0 +1,19 @@
+from distutils.command.config import config
+from functools import lru_cache
+
+from app.core.settings.app import AppSettings
+from app.core.settings.base import AppEnvironmentType, BaseAppSettings
+from app.core.settings.development import DevAppSettings
+
+environments: dict[AppEnvironmentType, type[AppSettings]] = {
+    AppEnvironmentType.dev: DevAppSettings,
+}
+
+
+@lru_cache
+def get_app_settings() -> AppSettings:
+    """Get app environment setting"""
+    base = BaseAppSettings()
+    app_env = base.ENVIRONMENT
+    config = environments[app_env]
+    return config()
