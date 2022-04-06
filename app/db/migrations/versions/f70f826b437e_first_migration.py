c67c86fcecf36f33b6fb616bdcc38763a761b4fb
helter daniel <helterdaniel@hotmail.com>
2022-04-06 15:23:20 -0300

    configura alembic
    Configuração do  alembic para migrações

---
+++ app/db/migrations/versions/f70f826b437e_first_migration.py
@@ -0,0 +1,24 @@
+"""first migration
+
+Revision ID: f70f826b437e
+Revises: 
+Create Date: 2022-04-06 15:21:33.701941
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f70f826b437e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
