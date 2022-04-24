8820d7706d3776369e43e15ded958bdb6bf9724b
helter daniel <helterdaniel@hotmail.com>
2022-04-23 22:39:40 -0300

    adiciona table de usuarios

--- app/db/tables/user.py
+++ app/db/tables/user.py
@@ -12,3 +12,8 @@ class Users(Base):
     is_superuser = Column(Boolean, default=False)
 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.current_timestamp(),
+    )
