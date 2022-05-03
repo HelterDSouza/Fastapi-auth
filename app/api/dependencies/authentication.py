bde39e839f206817478783fef43e8928e00354b0
helter daniel <helterdaniel@hotmail.com>
2022-05-03 16:04:03 -0300

    adiciona retorno do usuario atual

---
+++ app/api/dependencies/authentication.py
@@ -0,0 +1,105 @@
+from typing import Callable, Optional
+
+from app.api.dependencies.database import get_repository
+from app.core.config import get_app_settings
+from app.core.settings.app import AppSettings
+from app.db.erros import UserDoesNotExit
+from app.db.repositories.users import UsersRepository
+from app.models.domain.users import User
+from app.services import jwt
+from fastapi import Depends, HTTPException, Security
+from fastapi.security import APIKeyHeader
+from starlette import requests, status
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+HEADER_KEY = "Authorization"
+
+
+class ApiKeyHeader(APIKeyHeader):
+    async def __call__(self, request: requests.Request) -> Optional[str]:
+        try:
+            return await super().__call__(request)
+        except StarletteHTTPException as original_auth_exc:
+            raise HTTPException(
+                status_code=original_auth_exc.status_code,
+                detail="Authentication required",
+            )
+
+
+def get_current_user_authorizer(*, required: bool = True) -> Callable:
+    if required:
+        _get_current_user
+
+    return _get_current_user_optional
+
+
+def _get_authorization_header_retriever(*, required: bool = True) -> Callable:
+    if required:
+        return _get_authorization_header
+    return _get_authorization_header_optional
+
+
+def _get_authorization_header_optional(
+    authorization: str
+    | None = Security(
+        ApiKeyHeader(
+            name=HEADER_KEY,
+            auto_error=False,
+        )
+    ),
+    settings: AppSettings = Depends(get_app_settings),
+) -> str:
+    if authorization:
+        return _get_authorization_header(authorization, settings)
+    return ""
+
+
+def _get_authorization_header(
+    api_key: str = Security(ApiKeyHeader(name=HEADER_KEY)),
+    settings: AppSettings = Depends(get_app_settings),
+) -> str:
+    try:
+        token_prefix, token = api_key.split(" ")
+    except ValueError:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="wrong token prefix"
+        )
+    if token_prefix != settings.JWT_TOKEN_PREFIX:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="wrong token prefix"
+        )
+
+    return token
+
+
+async def _get_current_user(
+    user_repo: UsersRepository = Depends(get_repository(UsersRepository)),
+    token: str = Depends(_get_authorization_header_retriever),
+    settings: AppSettings = Depends(get_app_settings),
+) -> User:
+    try:
+        email = jwt.get_email_from_token(
+            token, str(settings.SECRET_KEY.get_secret_value())
+        )
+    except ValueError:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Malformed_payload"
+        )
+
+    try:
+        return await user_repo.get_user_by_email(email=email)
+    except UserDoesNotExit:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Malformed_payload"
+        )
+
+
+async def _get_current_user_optional(
+    repo: UsersRepository = Depends(get_repository(UsersRepository)),
+    token: str = Depends(_get_authorization_header_retriever(required=False)),
+    settings: AppSettings = Depends(get_app_settings),
+) -> Optional[User]:
+    if token:
+        return await _get_current_user(repo, token, settings)
+
+    return None
--- app/db/repositories/users.py
+++ app/db/repositories/users.py
@@ -1,14 +1,23 @@
+from app.db.erros import UserDoesNotExit
 from app.db.queries.queries import queries
 from app.db.repositories.base import BaseRepository
-from app.models.domain.users import UserInDB
-from app.models.schemas.users import UserInCreate
+from app.models.domain.users import User, UserInDB
 from asyncpg.connection import Connection
+from pydantic import EmailStr
 
 
 class UsersRepository(BaseRepository):
     def __init__(self, conn: Connection):
 
         super().__init__(conn)
+
+    async def get_user_by_email(self, *, email: EmailStr) -> UserInDB:
+        user_row = await queries.get_user_by_email(self.connection, email=email)
+
+        if user_row:
+            return UserInDB(**user_row)
+
+        raise UserDoesNotExit(f"user with email {email} does not exist")
 
     async def register_new_user(self, *, new_user: UserInCreate) -> UserInDB:
         user = UserInDB(email=new_user.email)
