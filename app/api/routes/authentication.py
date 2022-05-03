fc3c99a83707e3afc9ed1f3fbe2fe7ec9de12598
helter daniel <helterdaniel@hotmail.com>
2022-05-03 16:06:39 -0300

    adiciona rota de login

--- app/api/routes/authentication.py
+++ app/api/routes/authentication.py
@@ -18,6 +18,40 @@ from starlette.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 router = APIRouter()
 
 
+@router.post("/login", response_model=UserInResponse, name="auth:login")
+async def login(
+    user_login: UserInLogin = Body(..., embed=True, alias="user"),
+    user_repo: UsersRepository = Depends(get_repository(UsersRepository)),
+    settings: AppSettings = Depends(get_app_settings),
+) -> UserInResponse:
+
+    # Raise error from wrong login input
+    wrong_login_input_error = HTTPException(
+        status_code=HTTP_400_BAD_REQUEST, detail=string.INCORRECT_LOGIN_INPUT
+    )
+
+    try:
+        user = await user_repo.get_user_by_email(email=user_login.email)
+    except UserDoesNotExit as existence_error:
+        raise wrong_login_input_error from existence_error
+
+    # Check user password
+    if not user.check_password(user_login.password):
+        raise wrong_login_input_error
+
+    # Create Login token
+    token = jwt.create_access_token_for_user(
+        user, str(settings.SECRET_KEY.get_secret_value())
+    )
+
+    user_response = UserInResponse(
+        user=UserWithTokenInResponse(
+            **user.dict(),
+            token=token,
+        )
+    )
+
+    return user_response
 
 
 @router.post(
